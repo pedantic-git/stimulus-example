c5da888bb7b855cf239a48e1a55563da21c936e9
Quinn Daley <quinn.daley@citizensadvice.org.uk>
2023-09-15 15:33:57 +0100

    Attach a controller to the element that adds the enabled class

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static classes = [ "enabled" ]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hi")
+    this.element.classList.add(this.enabledClass)
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import DisclosureController from "./disclosure_controller"
+application.register("disclosure", DisclosureController)
