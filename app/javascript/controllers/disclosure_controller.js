60baa15b4ffc96a35b87243853f6dfb7952c0cc4
Quinn Daley <quinn.daley@citizensadvice.org.uk>
2023-09-15 15:50:07 +0100

    Nice tidy callback

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -3,13 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static classes = [ "enabled" ]
 
-  static targets = [ "details" ]
+  static targets = [ "button", "details" ]
+
+  static values = {
+    open: Boolean
+  }
 
   connect() {
     this.element.classList.add(this.enabledClass)
   }
 
   toggle () {
-    this.detailsTarget.classList.toggle("cads-disclosure__details--open")
+    this.openValue = !this.openValue
+  }
+
+  openValueChanged (newValue) {
+    if (newValue) {
+      this.buttonTarget.classList.replace("cads-icon_plus", "cads-icon_minus")
+      this.detailsTarget.classList.add("cads-disclosure__details--open")
+    } else {
+      this.buttonTarget.classList.replace("cads-icon_minus", "cads-icon_plus")
+      this.detailsTarget.classList.remove("cads-disclosure__details--open")
+    }
   }
 }
